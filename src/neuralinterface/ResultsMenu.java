1fc62ba948e65bf934bc808a7f766108f32fb46f
Gabriel <Gabriel@Gabriel-PC>
2017-01-29 23:53:38 -0200

    Ajustes realizados na tela de treinamento

--- src/neuralinterface/ResultsMenu.java
+++ src/neuralinterface/ResultsMenu.java
@@ -44,17 +44,9 @@ public class ResultsMenu extends javax.swing.JFrame {
 
             },
             new String [] {
-                "NOME", "C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10", "C11", "C12", "C13", "C14"
+                "NOME", "CLASSES"
             }
-        ) {
-            boolean[] canEdit = new boolean [] {
-                true, true, true, true, true, true, true, true, true, true, true, false, true, true, false
-            };
-
-            public boolean isCellEditable(int rowIndex, int columnIndex) {
-                return canEdit [columnIndex];
-            }
-        });
+        ));
         jScrollPane1.setViewportView(jTable1);
 
         jButton1.setText("OK");
